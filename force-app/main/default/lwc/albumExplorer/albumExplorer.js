f54bac67405c82a45d998872685629eb0fc06ae0
Carolina Larroque <55190406+cxrals@users.noreply.github.com>
2025-02-08 18:09:23 -0300

    fix

--- force-app/main/default/lwc/albumExplorer/albumExplorer.js
+++ force-app/main/default/lwc/albumExplorer/albumExplorer.js
@@ -1,38 +1,48 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 
-// statically analyzable dynamic import
 const COMPONENT_MAP = {
     tile: () => import('c/albumTileList'),
     table: () => import('c/albumListDataTable')
-}
+};
 
 export default class AlbumExplorer extends LightningElement {
     componentConstructor;
-    @api displayType = "tile";
+
+    @track _displayType = "tile";
 
     @wire(CurrentPageReference)
     currentPageRef;
 
+    @api
+    get displayType() {
+        return this._displayType;
+    }
+
+    set displayType(value) {
+        this._displayType = value;
+        this.loadComponent();
+    }
+
     async connectedCallback() {
-        const ctor = await COMPONENT_MAP[this.displayType]();
-        this.componentConstructor = ctor.default;
+        this.loadComponent();
+    }
+
+    async loadComponent() {
+        if (COMPONENT_MAP[this._displayType]) {
+            const ctor = await COMPONENT_MAP[this._displayType]();
+            this.componentConstructor = ctor.default;
+        }
     }
 
     get options() {
         return [
-            {label: "Tile", value: "tile"},
-            {label: "Table", value: "table"}
-        ]
+            { label: "Tile", value: "tile" },
+            { label: "Table", value: "table" }
+        ];
     }
 
     async handleChange(event) {
         this.displayType = event.detail.value;
-        const ctor = await COMPONENT_MAP[this.displayType]();
-        this.componentConstructor = ctor.default;
-    }
-
-    get displayType() {
-        return this.currentPageRef.state.c__displayType;
     }
 }
